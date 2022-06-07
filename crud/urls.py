749e3fe2742cd6233bf325ba09349fbd4b364d3d
Marco Aurélio Mendes <68308754+marcomendesbidula@users.noreply.github.com>
2022-06-07 13:24:03 -0300

    instalação de template e feito a parte html e bootstrap do projeto

--- crud/urls.py
+++ crud/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from crudapp.views import home
+from crudapp.views import home, form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('form/', form)
 ]
