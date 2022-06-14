724638e47ccc24eb517e1752fba5a644020bf195
Marco Aurélio Mendes <68308754+marcomendesbidula@users.noreply.github.com>
2022-06-13 22:28:46 -0300

    implementado o delete e ajax do projeto

--- crudapp/models.py
+++ crudapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Carros(models.Model):
+    objects = None
     modelo = models.CharField(max_length=150)
     marca = models.CharField(max_length=100)
     ano = models.IntegerField()
